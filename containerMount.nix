3e566643d979a8562e7b81de6ec11f00c42f42e7
Carl Andersson <git@lillecarl.com>
2025-09-10 03:45:19 +0200

    Work on minimal init script

--- containerMount.nix
+++ containerMount.nix
@@ -6,10 +6,19 @@ let
     ref = "nixos-unstable";
   };
   pkgs = import nixpkgs { };
+  lib = pkgs.lib;
+  folderPaths = [
+    "/tmp"
+    "/var/tmp"
+    "/var/log"
+    "/var/lib"
+    "/var/run"
+  ];
 in
 pkgs.buildEnv {
   name = "containerEnv";
   paths = with pkgs; [
+    curl
     coreutils
     fish
     execline
@@ -20,6 +29,9 @@ pkgs.buildEnv {
       export PATH "/nix/var/result/bin"
       foreground { echo "Initializing" }
       foreground { echo "Sleeping for infinity" }
+      ${lib.concatMapStringsSep "\n" (folder: "foreground { mkdir --parents ${folder} }") folderPaths}
+      foreground { ln --symbolic ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-bundle.crt }
+      foreground { ln --symbolic ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt /etc/ssl/certs/ca-certificates.crt }
       exec sleep infinity
     '')
   ];
