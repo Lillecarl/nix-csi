f52621074eb892e5151920b29f23371690488a36
Carl Andersson <git@lillecarl.com>
2025-09-30 05:17:58 +0200

    Remove fetch-tree  experimental toggle
    Todo: How do we do this across Nix versions?

--- kubenix/tmpDeploy.nix
+++ kubenix/tmpDeploy.nix
@@ -37,7 +37,7 @@ in
       data."nix.conf" = ''
         build-users-group = root
         auto-allocate-uids = true
-        experimental-features = nix-command flakes auto-allocate-uids fetch-tree fetch-closure
+        experimental-features = nix-command flakes auto-allocate-uids fetch-closure
       '';
     };
     kubernetes.resources.daemonSets.testd = {
