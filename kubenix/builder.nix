5b238242fa5ecc65ed176e0b65fbdc3616d04841
lillecarl <git@lillecarl.com>
2026-01-16 01:28:59 +0000

    map for string context discard

--- kubenix/builder.nix
+++ kubenix/builder.nix
@@ -163,8 +163,8 @@ in
                   volumeAttributes = {
                     # Only render storePaths here, building is done with a ConfigMap (config.nix) only if cfg.push is set
                     # this is so users don't have to build locally to deploy.
-                    x86_64-linux = builtins.unsafeDiscardStringContext x86Pkgs.nix-csi-builder-env;
-                    aarch64-linux = builtins.unsafeDiscardStringContext armPkgs.nix-csi-builder-env;
+                    x86_64-linux = x86Pkgs.nix-csi-builder-env;
+                    aarch64-linux = armPkgs.nix-csi-builder-env;
                   };
                 };
                 nix-store.emptyDir = { };
@@ -253,8 +253,8 @@ in
                             volumeAttributes = {
                               # Only render storePaths here, building is done with a ConfigMap (config.nix) only if cfg.push is set
                               # this is so users don't have to build locally to deploy.
-                              x86_64-linux = builtins.unsafeDiscardStringContext x86Pkgs.nix-csi-proxy-env;
-                              aarch64-linux = builtins.unsafeDiscardStringContext armPkgs.nix-csi-proxy-env;
+                              x86_64-linux = x86Pkgs.nix-csi-proxy-env;
+                              aarch64-linux = armPkgs.nix-csi-proxy-env;
                             };
                           };
 
--- kubenix/cache.nix
+++ kubenix/cache.nix
@@ -128,8 +128,8 @@ in
                     volumeAttributes = {
                       # Only render storePaths here, building is done with a ConfigMap (config.nix) only if cfg.push is set
                       # this is so users don't have to build locally to deploy.
-                      x86_64-linux = builtins.unsafeDiscardStringContext x86Pkgs.nix-csi-cache-env;
-                      aarch64-linux = builtins.unsafeDiscardStringContext armPkgs.nix-csi-cache-env;
+                      x86_64-linux = x86Pkgs.nix-csi-cache-env;
+                      aarch64-linux = armPkgs.nix-csi-cache-env;
                     };
                   };
 
